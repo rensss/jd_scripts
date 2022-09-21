8112b6702565911c025d83c60f3c3b7b8d9b517b
yishengv <zfm6633@Gmail.com>
2022-09-21 17:21:14 +0800

    新增去除微博发现页轮播图广告-测试

---
+++ others/weibo/weibo_removeAds.js
@@ -0,0 +1,24 @@
+/**
+ * 参考@zmqcherish的脚本
+ * 删除发现页的轮播广告图
+ * https://api.weibo.cn/2/search/finder
+ *
+ * 图片的链接有明显的区别
+ * 正常的图片url是这样的 "pic": "https://simg.s.weibo.com/ads/daka_1663682707.png",
+ * 广告的图片url是这样的 "pic": "https://wx4.sinaimg.cn/large/0024cZx9ly8h6cvd7kzk9j60ji08ijty02.jpg",
+ */
+
+function modifyMain(data) {
+    data = JSON.parse(data);
+    if (data.channelInfo.channels[0].payload.items[2]) {
+        data.channelInfo.channels[0].payload.items[2] = {};
+        return JSON.stringify(data);
+    }
+    return data;
+}
+
+var body = $response.body;
+
+body = modifyMain(body);
+
+$done({body});
