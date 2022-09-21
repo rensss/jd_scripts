c2202b92a4409feb2040e4d13e8deb6e7f606457
feiming <zfm6633@gmail.com>
2022-09-21 19:16:46 +0800

    优化微博去掉发现页轮播图模块

--- others/weibo/weibo_removeAds.js
+++ others/weibo/weibo_removeAds.js
@@ -1,9 +1,9 @@
 /**
- * 参考@zmqcherish的脚本
- * 删除发现页的轮播广告图
+ * 参考@zmqcherish 的脚本
+ * 删除发现页的轮播广告图(对比了广告和正常的数据，没有区别，所以直接删掉轮播图模块)
  * https://api.weibo.cn/2/search/finder
  *
- * 图片的链接有明显的区别
+ * 图片的链接有明显的区别（存在误杀，放弃）
  * 正常的图片url是这样的 "pic": "https://simg.s.weibo.com/ads/daka_1663682707.png",
  * 广告的图片url是这样的 "pic": "https://wx4.sinaimg.cn/large/0024cZx9ly8h6cvd7kzk9j60ji08ijty02.jpg",
  */
